b48ce55941fb73204c71fa29947cccc574c3867e
784594 <784594@pdsb.net>
2026-05-02 15:12:51 -0400

    Delete Hackathon directory

--- Hackathon/config.js
+++
@@ -1,9 +0,0 @@
-window.MEDIBAND_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyD6T0CAZlxS4BkFwmL-qrbEKTUkZmtDjmQ",
-  authDomain: "mediband-hackathon.firebaseapp.com",
-  databaseURL: "https://mediband-hackathon-default-rtdb.firebaseio.com/",
-  projectId: "mediband-hackathon",
-  storageBucket: "mediband-hackathon.firebasestorage.app",
-  messagingSenderId: "610227605868",
-  appId: "1:610227605868:web:d033c7083ef349475b6349"
-};
